eb666a2f72969fc65e08da1bef388a6eecb723fe
DineshMane2 <dineshmane272@gmail.com>
2023-07-31 22:47:42 +0530

    While loop assignment
    While loop assignment

---
+++ 06_while_Assignment02.js
@@ -0,0 +1,42 @@
+console.log("==============step 1 Write a nuber from 5 to 15 by incrementing 1====================");
+var i=5;
+while (i<=15) {
+    console.log(i);
+    i++
+}
+console.log("==============step 2 write a number from 50 to 40 by decremnting 1====================");
+var i=50;
+while (i>=40) {
+    console.log(i);
+    i--
+}
+console.log("==============step 3 write a code to get first 15 odd number ====================");
+var i=1;
+while (i<=29) {
+    console.log(i);
+    i+=2
+}
+console.log("==============step 4 write a code to get first 10 even number ====================");
+var i=0;
+while (i<=18) {
+    console.log(i);
+    i+=2
+}
+console.log("==============step 5 write a code to get a table of 5====================");
+var i=5;
+while (i<=50) {
+    console.log(i);
+    i+=5
+}
+console.log("==============step 6 write a code to get a table of 10 ====================");
+var i=10;
+while (i<=100) {
+    console.log(i);
+    i+=10
+} 
+console.log("==============step 7 write a code to get a table of 10 in reverse way ====================");
+var i=100;
+while (i>=10) {
+    console.log(i);
+    i-=10
+}
